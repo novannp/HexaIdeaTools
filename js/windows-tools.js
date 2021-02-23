f1b6972eef3f2b89bb9ca62467557ff18e45e3c6
coffeecoding07 <76527257+coffeecoding07@users.noreply.github.com>
2021-02-23 18:06:30 +0700

    HexaIdeaTools

---
+++ js/windows-tools.js
@@ -0,0 +1,44 @@
+const windowsTools = [{
+        name: "Figma",
+        desc: "Figma adalah lorem blabla sadkj aslkkdjasl djsalk jdaslkkdj aslj dlaskkj dlaskj jdlasj dlasjdlsajdlasjdlasjdlasjdlasjdlasjdlajdlajddlak ",
+        os: "Windows"
+        type: "Gratis" //gratis/berbayar
+        download: "https://figma.com"
+    },
+    {
+        name: "",
+        desc: "",
+        type: "",
+        download: ""
+    },
+    {
+        name: "",
+        desc: "",
+        type: "",
+        download: ""
+    },
+    {
+        name: "",
+        desc: "",
+        type: "",
+        download: ""
+    },
+    {
+        name: "",
+        desc: "",
+        type: "",
+        download: ""
+    },
+    {
+        name: "",
+        desc: "",
+        type: "",
+        download: ""
+    },
+    {
+        name: "",
+        desc: "",
+        type: "",
+        download: ""
+    },
+]
